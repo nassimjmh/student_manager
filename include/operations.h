5b9b5a29dd034cc457684b2804dd4dea221dd216
nassimjmh <n455im07@gmail.com>
2025-12-17 13:25:47 +0100

    teste

--- include/operations.h
+++ include/operations.h
@@ -5,5 +5,12 @@
 
 typedef void (*Recalcul)(Student* student, int max, int min);
 
+typedef struct recalcul {
+    Recalcul func;
+    float max;
+    float min;
+} Recalcul;
+
+int miseAJourNotes(Prom* prom, Recalcul* pRecalcul);
 
 #endif /* OPERATIONS_H */
--- src/operations.c
+++ src/operations.c
@@ -6,14 +6,18 @@
 
 
 int miseAJourNotes(Prom* prom, Recalcul* pRecalcul){
-    // Recherche de moyenne extremes
     if (prom == NULL || pRecalcul == NULL) {
         return -1; 
     }
+    if (pRecalcul->max>20 || pRecalcul->min<0)
+    // Recherche de moyenne extremes
+    for (int i = 0; i < prom->number; i++) {
+        if(&prom->students->avg>&pRecalcul->max || &prom->students->avg<&pRecalcul->min){
+            pRecalcul->func(&prom->students[i], pRecalcul->max, pRecalcul->min);
+        }
+    
     
 
-
-
-
-
+    }
+    return 1;
 }
