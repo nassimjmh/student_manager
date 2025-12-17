5b77904d31b5fb75331f4554d955dfcfcc51fe5e
nassimjmh <n455im07@gmail.com>
2025-12-17 13:16:56 +0100

    teste

---
+++ include/operations.h
@@ -0,0 +1,9 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+#include "structures.h"
+
+typedef void (*Recalcul)(Student* student, int max, int min);
+
+
+#endif /* OPERATIONS_H */
---
+++ src/operations.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../include/operations.h"
+
+
+int miseAJourNotes(Prom* prom, Recalcul* pRecalcul){
+    // Recherche de moyenne extremes
+    if (prom == NULL || pRecalcul == NULL) {
+        return -1; 
+    }
+    
+
+
+
+
+
+}
